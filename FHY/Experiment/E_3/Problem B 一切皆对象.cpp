56308619e6efa8caa4f183a72ff66f2708056801
wuguangkai211 <53203590+wuguangkai211@users.noreply.github.com>
2020-04-26 22:25:20 +0800

    Add files via upload

---
+++ FHY/Experiment/E_3/Problem B 一切皆对象.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Thing
+{
+private:
+    string ss$;
+public:
+    Thing(string tt):ss$(tt){cout << "A thing named by " << ss$ << " is created!" << endl;}
+    Thing(){cout << "A thing without name is created!" << endl;}
+    Thing(const Thing &tt):ss$(tt.ss$)
+    {
+        if(ss$.empty())//ÅÐ¶ÏstringÊÇ·ñÎª¿Õ
+        {
+            cout << "A thing without name is copied!" <<endl;
+        }
+        else
+        {
+             cout << "A thing named by " << ss$ << " is copied!" <<endl;
+        }
+    }
+    ~Thing()
+    {
+        if(ss$.empty())
+        {
+            cout << "A thing without name is erased!" <<endl;
+        }
+        else
+        {
+             cout << "A thing named by " << ss$ << " is erased!" <<endl;
+        }
+    }
+
+
+
+
+
+
+
+};
+
+
+
+
+
+
+int main()
+{
+    string name;
+    Thing Thing1, Thing2(Thing1);
+    cin>>name;
+    Thing Thing3(name);
+    Thing Thing4(Thing3);
+    return 0;
+}
