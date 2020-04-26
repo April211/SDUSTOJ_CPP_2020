834d6f7c5939fdef61608cc42d6f726724f62fbc
wuguangkai211 <53203590+wuguangkai211@users.noreply.github.com>
2020-04-26 22:45:21 +0800

    Update HelloWorld.cpp

--- FHY/Homework/H_1/HelloWorld.cpp
+++ FHY/Homework/H_1/HelloWorld.cpp
@@ -13,3 +13,28 @@ int main()
 
     return 0;
 }
+
+
+//Problem A: Hello world!
+//Time Limit: 1 Sec  Memory Limit: 2 MB
+//Submit: 23796  Solved: 12917
+//[Submit][Status]
+//Description
+//Xiao_ming有两个哥哥，大哥叫Da_min，二哥叫Er_min。三兄弟放学回家，父母分别跟他们打招呼。
+
+//Input
+//无
+
+//Output
+//请输出：
+//Hello Da_min,
+//Hello Er_min,
+//Hello Xiao_ming!
+
+//Sample Input
+//Sample Output
+//Hello Da_min,
+//Hello Er_min,
+//Hello Xiao_ming!
+//HINT
+//请注意换行符
