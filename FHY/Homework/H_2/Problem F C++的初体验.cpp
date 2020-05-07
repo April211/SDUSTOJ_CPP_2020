5952e49825d582289e54c21e67bb8f8b806f8f56
wuguangkai211 <53203590+wuguangkai211@users.noreply.github.com>
2020-05-07 11:57:26 +0800

    Update Problem F C++的初体验.cpp

--- FHY/Homework/H_2/Problem F C++的初体验.cpp
+++ FHY/Homework/H_2/Problem F C++的初体验.cpp
@@ -25,3 +25,35 @@ int main()
 
     return 0;
 }
+
+// Problem F: C++的初体验
+// Time Limit: 1 Sec  Memory Limit: 128 MB
+// Submit: 651  Solved: 347
+// [Submit][Status]
+// Description
+// 通过cin，cout进行C++程序的输入和输出
+
+// Input
+// 1.输入一个整数a
+// 2.输入一个浮点数b
+// 3.输入一个长整型数c
+// 4.输入一个整数d
+
+// Output
+// 第一行输出整数a
+// 第二行输出浮点数b，保留小数点后7位
+// 第三行输出长整型数c
+// 第四行输出整数d，宽度为16，右对齐
+
+// Sample Input
+// 1
+// 2.323
+// 2314213435435543
+// 213
+// Sample Output
+// 1
+// 2.3230000
+// 2314213435435543
+//              213
+// HINT
+// Append Code
