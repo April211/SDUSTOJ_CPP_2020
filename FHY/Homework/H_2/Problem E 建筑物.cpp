a7891cc42b2160c757bc66af38c376893fa44b0c
wuguangkai211 <53203590+wuguangkai211@users.noreply.github.com>
2020-05-07 11:56:53 +0800

    Update Problem E 建筑物.cpp

--- FHY/Homework/H_2/Problem E 建筑物.cpp
+++ FHY/Homework/H_2/Problem E 建筑物.cpp
@@ -28,3 +28,26 @@ int main()
     return 0;
 }
 
+// Problem E: 建筑物
+// Time Limit: 1 Sec  Memory Limit: 128 MB
+// Submit: 1343  Solved: 1039
+// [Submit][Status]
+// Description
+// 定义一个类Building，其中只有一个int属性cnt，表示建筑物的层数。有两个方法：
+// 1.void setCnt(int c)：设置楼层数为c。
+// 2.int getCnt()：获得楼层数。
+
+// Input
+// 一个非负整数。
+
+// Output
+// 见样例。
+
+// Sample Input
+// 3
+// Sample Output
+// The building has 3 floors.
+// HINT
+// Append Code
+// append.cc,
+
