6b7d456cd154ea1bffb5149eeea2fd0ffea35e89
wuguangkai211 <wuguangkai2018@outlook.com>
2020-09-01 17:49:40 +0800

    clear只是将元素擦除，并未实际释放内存

--- FHY/Else/vector_capacity_size.cpp
+++ FHY/Else/vector_capacity_size.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 using namespace std;
 
+
 int main()
 {
     vector<int> t(30, 1);    //开辟30个元素的空间，并全部赋值为 “1”
@@ -13,12 +14,18 @@ int main()
     cout << "The value of the 31st element: " << t[30] << endl;
     cout << t.capacity() << endl;
     cout << t.size() << endl;
+    t.clear();
+    cout << t.capacity() << endl;
+    cout << t.size() << endl;
     return 0;
 }
 
+/* size返回的是实际存入的数据的个数；capacity返回的是容量，即能够容纳数据的最大个数 */
 /* The value of the 30th element: 1
 30
 30
 The value of the 31st element: 5
 60
-31 */
+31
+60
+0 */
