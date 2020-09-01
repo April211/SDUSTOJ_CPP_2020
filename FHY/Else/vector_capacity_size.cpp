cd52590f46f60a933cb613c02d048136ce2be7b1
wuguangkai211 <wuguangkai2018@outlook.com>
2020-09-01 17:19:54 +0800

    添加更多输出信息

--- FHY/Else/vector_capacity_size.cpp
+++ FHY/Else/vector_capacity_size.cpp
@@ -2,12 +2,15 @@
 #include <vector>
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    vector<int> t(30);
+    vector<int> t(30, 1);    //开辟30个元素的空间，并全部赋值为 “1”
+    cout << "The value of the 30th element: " << t[29] << endl;
     cout << t.capacity() << endl;
     cout << t.size() << endl;
-    t.push_back(5);
+    t.push_back(5);          //在末尾添加一个元素为 "5"
+    cout << "The value of the 31st element: " << t[30] << endl;
     cout << t.capacity() << endl;
     cout << t.size() << endl;
     return 0;
